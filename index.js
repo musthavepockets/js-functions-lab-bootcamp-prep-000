f49f5629cddc81f7c10b08ec92499c98fd850d38
cd <coraldarling@outlook.com>
2018-07-02 16:53:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ function happyHolidays()  {
   return ('Happy Holidays!')
 }
 
+happyHolidays()
+
 function happyHolidaysTo()  {
   return `Happy Holidays, ${name}!`
 }
+
+happyHolidaysTo('Kenji')
 
 function happyHolidayTo()  {
   return `Happy ${holiday}, ${name}!`
 }
 
+happyHolidayTo('Hanukkah', 'Sarah')
+
 function holidayCountdown()  {
   return `It\'s ${days} days until ${holiday}!`
 }
+
+holidayCountdown('8', 'Kwanza')
